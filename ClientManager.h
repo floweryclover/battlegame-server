aa726f9c220ceb5fc1ae20fc5347f9d1b162c302
floweryclover <floweryclover@gmail.com>
2024-01-30 23:29:58 +0900

    Add: ClientManager 클래스 추가

---
+++ ClientManager.h
@@ -0,0 +1,14 @@
+//
+// Created by floweryclover on 2024-01-30.
+//
+
+#ifndef BATTLEGAME_SERVER_CLIENTMANAGER_H
+#define BATTLEGAME_SERVER_CLIENTMANAGER_H
+
+
+class ClientManager {
+
+};
+
+
+#endif //BATTLEGAME_SERVER_CLIENTMANAGER_H
