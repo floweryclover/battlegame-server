483671380b504bfa7b2d8210c58696beefc718e5
floweryclover <floweryclover@gmail.com>
2024-01-30 16:25:15 +0900

    Add: 게임과 플레이어 정보를 저장할 GameData 클래스 추가

---
+++ GameData.h
@@ -0,0 +1,14 @@
+//
+// Created by floweryclover on 2024-01-30.
+//
+
+#ifndef BATTLEGAME_SERVER_GAMEDATA_H
+#define BATTLEGAME_SERVER_GAMEDATA_H
+
+
+class GameData {
+
+};
+
+
+#endif //BATTLEGAME_SERVER_GAMEDATA_H
