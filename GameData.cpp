4b209ea4655a22ee0ef5cf97ad4ded603c0e9566
floweryclover <floweryclover@gmail.com>
2024-01-30 16:56:53 +0900

    Implement: GameData 클래스 싱글톤 패턴 적용
    객체 임의 생성, 복사 및 이동 금지

--- GameData.cpp
+++ GameData.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "GameData.h"
+
+GameData& GameData::GetInstance()
+{
+    static GameData instance;
+    return instance;
+}
+
+void GameData::OnPlayerDisconnected(ConnectionId id)
+{
+    mNicknames.erase(id);
+}
+
+void GameData::OnPlayerConnected(ConnectionId id)
+{
+    mNicknames.emplace(id, std::string("플레이어"));
+}
+
+bool GameData::SetPlayerNickname(ConnectionId id, const std::string& nickname)
+{
+    if (mNicknames.contains(id))
+    {
+        mNicknames[id] = nickname;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
--- GameData.h
+++ GameData.h
@@ -5,9 +5,28 @@
 #ifndef BATTLEGAME_SERVER_GAMEDATA_H
 #define BATTLEGAME_SERVER_GAMEDATA_H
 
+#include <map>
+#include <string>
+#include <optional>
+#include <expected>
+
+using ConnectionId = unsigned int;
 
 class GameData {
+public:
+    GameData(GameData&& rhs) = delete;
+    GameData(const GameData& rhs) = delete;
+    GameData& operator=(const GameData& rhs) = delete;
 
+    static GameData& GetInstance();
+    void OnPlayerConnected(ConnectionId id);
+    void OnPlayerDisconnected(ConnectionId id);
+
+    bool SetPlayerNickname(ConnectionId id, const std::string& nickname);
+    inline const std::string* GetPlayerNickname(ConnectionId id) { return mNicknames.contains(id) ? &mNicknames[id] : nullptr; }
+private:
+    GameData() = default;
+    std::map<ConnectionId, std::string> mNicknames;
 };
 
 
