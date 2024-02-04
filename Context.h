912ccb9a17da57387be456340499780e4b0fa5e5
floweryclover <floweryclover@gmail.com>
2024-02-05 00:54:52 +0900

    Feat: Context가 전송받은 메시지의 신뢰성을 알 수 있도록 멤버와 생성자 설정

--- Context.h
+++ Context.h
@@ -5,17 +5,21 @@
 #ifndef BATTLEGAME_SERVER_CONTEXT_H
 #define BATTLEGAME_SERVER_CONTEXT_H
 
+#include "Enums.h"
+
 using SerializedEndpoint = unsigned long long;
 using ClientId = SerializedEndpoint;
 
 class Context {
 public:
-    explicit Context(const ClientId clientId) : mClientId(clientId) {};
+    explicit Context(ClientId clientId, SendReliability reliability) : mClientId(clientId), mReliability(reliability) {};
     ~Context() = default;
 
     inline ClientId GetClientId() const { return mClientId; }
+    inline SendReliability GetReliability() const { return mReliability; }
 private:
     const ClientId mClientId;
+    const SendReliability mReliability;
 };
 
 
