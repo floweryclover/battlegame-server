8e187a8875c4a69c9be848596e758bf32facf0ec
floweryclover <floweryclover@gmail.com>
2024-02-05 00:52:12 +0900

    Add: 메시지 전송 신뢰성을 설정하는 데에 사용되는 SendReliability Enum 추가

---
+++ Enums.h
@@ -0,0 +1,14 @@
+//
+// Created by floweryclover on 2024-02-05.
+//
+
+#ifndef BATTLEGAME_SERVER_ENUMS_H
+#define BATTLEGAME_SERVER_ENUMS_H
+
+enum class SendReliability
+{
+    UNRELIABLE,
+    RELIABLE
+};
+
+#endif //BATTLEGAME_SERVER_ENUMS_H
